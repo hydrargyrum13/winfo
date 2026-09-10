1213e3ea5e3dd1e156423a40559bd88116a5703e
hydrargyrum13 <hydrargyrum13@proton.me>
2026-09-10 18:18:42 +0300

    Fix UTF-8 decoding on Windows PowerShell 5.1

--- winfo.cmd
+++ winfo.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0winfo.ps1" %*
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$p = '%~dp0winfo.ps1'; $s = [System.IO.File]::ReadAllText($p, [System.Text.UTF8Encoding]::new($false)); & ([ScriptBlock]::Create($s)) %*"
 exit /b %errorlevel%
